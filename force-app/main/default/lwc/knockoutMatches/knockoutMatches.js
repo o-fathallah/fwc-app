36a42a5223d551aae929dde77e09f3edeb65c1f6
o-fathallah <ofathallah@salesforce.com>
2025-03-18 22:00:49 +0000

    display knockout matches as blocks

--- force-app/main/default/lwc/knockoutMatches/knockoutMatches.js
+++ force-app/main/default/lwc/knockoutMatches/knockoutMatches.js
@@ -9,26 +9,26 @@ export default class KnockoutMatches extends LightningElement {
     wiredMatches({ error, data }) {
         if (data) {
             this.knockoutMatches = data;
+
             // Group matches by round
-            const roundsMap = {};
+            const roundMap = {};
             data.forEach(match => {
                 const round = match.Round__c;
-                if (!roundsMap[round]) {
-                    roundsMap[round] = { round: round, matches: [] };
+                if (!roundMap[round]) {
+                    roundMap[round] = { round: round, matches: [] };
                 }
-                roundsMap[round].matches.push(match);
+                roundMap[round].matches.push(match);
             });
-            // Convert the rounds object into an array
-            this.rounds = Object.values(roundsMap);
-            console.log(JSON.stringify(this.rounds));
-            // Sort rounds in desired order
-            const order = {
+            // Convert roundMap to an array and sort by the desired knockout order
+            const roundOrder = {
                 'Round of 16': 1,
                 'Quarter Finals': 2,
                 'Semi Finals': 3,
                 'Finals': 4
             };
-            this.rounds.sort((a, b) => order[a.round] - order[b.round]);
+            this.rounds = Object.values(roundMap).sort((a, b) => {
+                return roundOrder[a.round] - roundOrder[b.round];
+            });
         } else if (error) {
             console.error('Error fetching knockout matches:', error);
         }
